562b3fa6d5e4d6c73a5683b288e1ca152e390750
thehaiwave <manueld0293@gmail.com>
2020-08-07 13:59:45 -0500

    Added internet speed test

--- telmexcomplainer.py
+++ telmexcomplainer.py
@@ -1,1 +1,18 @@
-print("Nice")
+import os
+import sys
+import speedtest
+
+def test():
+    speedResult = speedtest.Speedtest()
+    speedResult.get_servers()
+    speedResult.get_best_server()
+    return speedResult.download()
+
+
+def main():
+    downloadSpeed = test()
+
+
+if __name__ == "__main__":
+    main()
+    print("Finalizado")
