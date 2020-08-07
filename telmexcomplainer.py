7a8b9c01dfe07a2815a9d19952023c05bc7d255e
thehaiwave <manueld0293@gmail.com>
2020-08-07 15:47:47 -0500

    Speed threshold logic

--- telmexcomplainer.py
+++ telmexcomplainer.py
@@ -9,30 +9,37 @@ def main():
     botInstance.run()
 
 
-class DownloadSpeedTest():
-    def __init__(self):
-        self.config = json.load(open('./config.json'))
-
-    def test(self):
-        speedResult = speedtest.Speedtest()
-        speedResult.get_servers()
-        speedResult.get_best_server()
-        return speedResult.upload()
-
-
 class ManageBot():
     def __init__(self):
         self.config = json.load(open('./config.json'))
         
     def run(self):
-        botSpeedTest = DownloadSpeedTest()
-        speedResults = botSpeedTest.test()
-        self.speedAssessment(speedResults)
+        speedTestResult = self.runSpeedTest()
+        self.makeTweet(speedTestResult)
     
+    def runSpeedTest(self):
+        speedResult = speedtest.Speedtest()
+        speedResult.get_servers()
+        speedResult.get_best_server()
+        return speedResult.upload()
+
     def speedAssessment(self, speedResults):
         speedResults = (speedResults/1024)/1024
         speedThreshold = self.config['internetSpeedThreshold']
 
+        if(speedResults < (speedThreshold*30)/100):
+            return "bad"
+
+        elif(speedResults < (speedThreshold*40)/100):
+            return "horrible"
+
+    def makeTweet(self, speedTestResult):
+        speedStatus = self.speedAssessment(speedTestResult)
+
+        try:
+            print(self.config["tweetSelection"][speedStatus])
+        except:
+            print("Not valid")
 
 
 if __name__ == "__main__":
