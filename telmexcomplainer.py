cb3fac20d2fcaa24456cbbf92ee7bfa6c463fa35
thehaiwave <manueld0293@gmail.com>
2020-08-07 15:07:20 -0500

    Added classes to manage the bot

--- telmexcomplainer.py
+++ telmexcomplainer.py
@@ -1,16 +1,38 @@
 import os
 import sys
 import speedtest
-
-def test():
-    speedResult = speedtest.Speedtest()
-    speedResult.get_servers()
-    speedResult.get_best_server()
-    return speedResult.download()
-
+import json
 
 def main():
-    downloadSpeed = test()
+    print("Iniciando")
+    botInstance = ManageBot()
+    botInstance.run()
+
+
+class DownloadSpeedTest():
+    def __init__(self):
+        self.config = json.load(open('./config.json'))
+
+    def test(self):
+        speedResult = speedtest.Speedtest()
+        speedResult.get_servers()
+        speedResult.get_best_server()
+        return speedResult.upload()
+
+
+class ManageBot():
+    def __init__(self):
+        self.config = json.load(open('./config.json'))
+        
+    def run(self):
+        botSpeedTest = DownloadSpeedTest()
+        speedResults = botSpeedTest.test()
+        self.speedAssessment(speedResults)
+    
+    def speedAssessment(self, speedResults):
+        speedResults = (speedResults/1024)/1024
+        speedThreshold = self.config['internetSpeedThreshold']
+
 
 
 if __name__ == "__main__":
