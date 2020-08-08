09ca1b0ceedae9652fa96bee396a6aea0dae2fbc
thehaiwave <manueld0293@gmail.com>
2020-08-07 19:18:46 -0500

    Added eror handling

--- telmexcomplainer.py
+++ telmexcomplainer.py
@@ -4,13 +4,25 @@ import speedtest
 import json
 import random
 import twitter
+import time
+import daemon
+from datetime import datetime, timedelta
 
 def main():
-    print("Iniciando")
+
     botInstance = ManageBot()
-    botInstance.run()
 
-
+    while True:
+        try:
+            botInstance.run()
+            dt = datetime.now() + timedelta(seconds=1)
+            dt = dt.replace(minute=10)
+            
+            while datetime.now() < dt:
+                time.sleep(1)
+        except:
+            sys.exit(1)
+            
 class ManageBot():
     def __init__(self):
         self.config = json.load(open('./config.json'))
@@ -29,31 +41,35 @@ class ManageBot():
         speedResults = (speedResults/1024)/1024
         speedThreshold = self.config['internetSpeedThreshold']
 
-        if(speedResults < (speedThreshold*80)/100):
+        if(speedResults < (speedThreshold*40)/100):
             return "bad"
-
-        elif(speedResults < (speedThreshold*90)/100):
+        elif(speedResults < (speedThreshold*50)/100):
             return "horrible"
+        else:
+            return None
 
     def makeTweet(self, speedTestResult):
         speedStatus = self.speedAssessment(speedTestResult)
         speedTestResult = round((speedTestResult/1024)/1024)
 
         try:
+            # If this fails it's because speedAssessment() returned None. Not really an error, the speed is just not low enough
+            # so there's really no harm in ignoring it.
+            # Should sendTweet() fail, the error is going to be thrown inside the function and not the call, so the program
+            # will stop because the bot instance is wrapped in a try-catch. Probably not the best way of handling an exception.
             message = self.config["tweetSelection"][speedStatus][random.randint(0,1)].replace('{speedTestResults}', str(speedTestResult)).replace('{atISP}', self.config["atISP"])
             self.sendTweet(message)
         except:
-            print("Not valid")
+            pass
 
     def sendTweet(self, message):
         api = twitter.Api(consumer_key=self.config["twitterAuth"]["twitterApiKey"],
                         consumer_secret=self.config["twitterAuth"]["twitterApiKeyScret"],
                         access_token_key=self.config["twitterAuth"]["twitterAccessToken"],
                         access_token_secret=self.config["twitterAuth"]["twitterAccessTokenSecret"])
-                        
+
         api.PostUpdate(message)
 
 
 if __name__ == "__main__":
     main()
-    print("Finalizado")
